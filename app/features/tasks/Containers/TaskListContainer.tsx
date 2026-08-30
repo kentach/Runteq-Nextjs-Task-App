98a1aa62bd94fb696388c907f7eca258d038cba3
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-08-30 12:24:55 +0900

    add/TaskListContainerのスタイルの追記

--- app/features/tasks/Containers/TaskListContainer.tsx
+++ app/features/tasks/Containers/TaskListContainer.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { getTasks } from "../hooks/getTasks";
 import type { Task } from "@/app/types/task";
 import TaskCard from "../components/TaskCard";
+import styles from "../styles/TaskListContainer.module.css";
 
 export default function TaskListContainer() {
   const [tasks, setTasks] = useState<Task[]>([]);
@@ -17,12 +18,23 @@ export default function TaskListContainer() {
   }, []);
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>タスク一覧</h1>
-      <div>
-        {tasks.map((task) => (
-          <TaskCard key={task.id} task={task} />
-        ))}
+      <div className={styles.tableContainer}>
+
+        <div className={styles.header}>
+          <div>タスク名</div>
+          <div>ステータス</div>
+          <div>優先度</div>
+          <div>期限日</div>
+        </div>
+
+        <div className={styles.taskList}>
+          {tasks.map((task) => (
+            <TaskCard key={task.id} task={task} />
+          ))}
+        </div>
+        
       </div>
     </div>
   );
